9449e2acc06ee9b53ff0b14179349988f0353f0f
1Cezzo <emiljohnzen@gmail.com>
2023-06-08 20:57:31 +0200

    Initial commit

---
+++ src/main/java/com/burstingcalculator/BurstingCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.burstingcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurstingCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurstingCalculatorApplication.class, args);
+	}
+
+}
