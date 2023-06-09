34dccaf7f6d3c26624c1ab8c4c08130a0a71127b
1Cezzo <emiljohnzen@gmail.com>
2023-06-09 02:33:39 +0200

    updating progress on xp-hour page

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,2 +1,38 @@
-// Add your JavaScript code here
-// (if you don't have any JavaScript functionality, you can leave this file empty)
+// Get the elements
+var dropdownContent = document.getElementById("dropdown-content");
+var searchInput = document.getElementById("search-input");
+var selectedItemText = document.getElementById("selected-item-text");
+
+// Function to toggle the dropdown
+function toggleDropdown() {
+    dropdownContent.classList.toggle("show");
+}
+
+// Function to filter the items based on search input
+function filterItems() {
+    var input = searchInput.value.toLowerCase();
+    var items = dropdownContent.getElementsByTagName("a");
+
+    for (var i = 0; i < items.length; i++) {
+        var itemText = items[i].textContent.toLowerCase();
+        if (itemText.indexOf(input) > -1) {
+            items[i].style.display = "";
+        } else {
+            items[i].style.display = "none";
+        }
+    }
+}
+
+// Function to set the selected item
+function selectItem(itemText, imageUrl) {
+    selectedItemText.textContent = itemText;
+    toggleDropdown();
+    updateHelmetImage(imageUrl);
+}
+
+// Function to update the helmet image
+function updateHelmetImage(imageUrl) {
+    var helmetImage = document.getElementById("helmet-image");
+    helmetImage.src = imageUrl;
+}
+
